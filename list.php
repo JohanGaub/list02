f0da929a768b9c2473107e04ed6cdf5f4d203073
Johan Gaub <johangaub@gmail.com>
2017-03-01 15:36:10 +0100

    "First commit on Github "

---
+++ list.php
@@ -0,0 +1,18 @@
+<?php
+
+$students = array(
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19
+); // values affected
+$sumAge = 0;
+
+
+foreach ($students as $nameKey => $ageValue){
+    echo "$nameKey : $ageValue" . PHP_EOL;
+
+    $sumAge += $ageValue;
+};
+echo "La moyenne d'age est de" . " " . $sumAge / count($students) . " ans" . PHP_EOL;
